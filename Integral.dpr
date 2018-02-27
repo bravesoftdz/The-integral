4cf49144d6e5dc7864ec6941e1c363083f81496f
Nikita <mineralsfree@gmail.com>
2018-02-27 10:21:30 +0300

    12
    21

--- Integral.dpr
+++ Integral.dpr
@@ -6,8 +6,36 @@ program Integral;
 
 uses
   System.SysUtils;
+  function Second_method(var n:Integer;eps:Real; a, b:Integer; kek:TFormula):real;
+  var
+  h:real;
+  xi:real;
+  LastL:Real;
+  I,L:real;
+  lol:real;
+  begin
+   N:=0;
+   LastL:=kek(a)*(b-a);
+  repeat
+  Inc(N,5);
+  L:=0;
+  h:=(b-a)/2;
+   for i := 1 to N do
+   begin
+   xi:=a+i*h;
+   L:=L+kek(xi);
+   end;
+  lol:=Abs(L-LastL);
+  LastL:=L
 
+
+  until Abs(lol)>eps ;
+
+
+  end;
 begin
+writeln(#$9 + 'First Method' + #$ +'Second method');
 
-
+Write('First integral');
 end.
+
